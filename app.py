0fc38dc65a7ff5243a101246f12efefef48d4287
MagicznyFrank <lolek3533@gmail.com>
2024-03-27 16:38:59 +0100

    Fixing the routing

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
-from flask import Flask
+from flask import Flask,redirect,url_for
 from views import views
 
 app = Flask(__name__)
 app.register_blueprint(views, url_prefix="/views")
+@app.route("/")
+def home():
+    return redirect(url_for('views.create'))
 
 if __name__ == '__main__':
     app.run(debug=True, port=8000)
