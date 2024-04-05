dc318b129567c01935a20c6414f81c30dac40f07
MagicznyFrank <lolek3533@gmail.com>
2024-04-05 15:00:39 +0200

    working on database

--- views.py
+++ views.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, request
+from flask import Blueprint, render_template, request, redirect, url_for
 from .database import execute_query, fetch_data
 import subprocess
 import logging
